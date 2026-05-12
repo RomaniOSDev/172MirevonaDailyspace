645af7b0e4e268dd826d5dd9d88fc3b64e322ca9
Roman <romazaniosdev@gmail.com>
2026-05-12 21:42:35 +0300

    add link

--- 172MirevonaDailyspace/Features/Settings/AppSettingsLink.swift
+++ 172MirevonaDailyspace/Features/Settings/AppSettingsLink.swift
@@ -6,8 +6,8 @@
 import Foundation
 
 enum AppSettingsLink: String {
-    case privacyPolicy = "https://example.com/privacy-policy"
-    case termsOfUse = "https://example.com/terms"
+    case privacyPolicy = "https://mirevona172dailyspace.site/privacy/177"
+    case termsOfUse = "https://mirevona172dailyspace.site/terms/177"
 
     var url: URL? {
         URL(string: rawValue)
